bc2b2a07ffffbdcde1d49bdbd71febcb51496d66
Teoman Palit <s142230@ap.be>
2025-10-22 01:46:52 +0200

    Test project added

--- Adventure-project/Adventure-project/obj/Debug/net8.0/Adventure-project.AssemblyInfo.cs
+++ Adventure-project/Adventure-project/obj/Debug/net8.0/Adventure-project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Adventure-project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2887abb41200ed545bfdec3983f077c05bed9a72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b1d08004e277101a0652eebce794148135cb01b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Adventure-project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Adventure-project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
