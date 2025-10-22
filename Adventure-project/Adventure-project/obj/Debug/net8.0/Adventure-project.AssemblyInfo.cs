5b4446729c4426295b6d2498580bb210f657b23f
Teoman Palit <s142230@ap.be>
2025-10-22 02:46:27 +0200

    Made UnitTest methods for Item.cs

--- Adventure-project/Adventure-project/obj/Debug/net8.0/Adventure-project.AssemblyInfo.cs
+++ Adventure-project/Adventure-project/obj/Debug/net8.0/Adventure-project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Adventure-project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b1d08004e277101a0652eebce794148135cb01b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc2b2a07ffffbdcde1d49bdbd71febcb51496d66")]
 [assembly: System.Reflection.AssemblyProductAttribute("Adventure-project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Adventure-project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
